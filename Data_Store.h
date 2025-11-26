1fae3817318f7ef2a683319cf6efb9a7e1b92905
TomasTrujillo369 <ttrujillozur.ieu2024@student.ie.edu>
2025-11-26 18:57:47 +0100

    Data_Store.h

--- Data_Store.h
+++ Data_Store.h
@@ -1,1 +1,17 @@
+// kvstore.h
+#ifndef KVSTORE_H
+#define KVSTORE_H
+
+#include <stddef.h>
+
+void kv_init(const char *filename);   // load from file if exists
+void kv_shutdown(void);               // save + free
+
+int  kv_set(const char *key, const char *value);
+int  kv_get(const char *key, char *out_value, size_t out_size);
+int  kv_del(const char *key);
+int  kv_keys(char *out_buf, size_t out_size);  // returns number of keys
+int  kv_save(void);                            // force save to file
+
+#endif
 
