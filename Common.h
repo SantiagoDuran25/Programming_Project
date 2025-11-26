ea1e7245bcbda77000e36cbdb4d9b6a82b9b02fd
TomasTrujillo369 <ttrujillozur.ieu2024@student.ie.edu>
2025-11-26 18:56:50 +0100

    Common.h

--- Common.h
+++ Common.h
@@ -1,1 +1,16 @@
+// common.h
+#ifndef COMMON_H
+#define COMMON_H
+
+#define MAX_CLIENTS  32
+#define BUF_SIZE     1024
+
+#define KEY_LEN      64
+#define VALUE_LEN    256
+#define HASH_SIZE    128
+
+#define DEFAULT_PORT 5001
+#define DUMP_FILE    "dump.kv"
+
+#endif
 
